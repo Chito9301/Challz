7244165c27310ebfb2f6f4dee33d603cd7c0703a
Chito9301 <85809089+Chito9301@users.noreply.github.com>
2025-08-05 22:27:12 -0400

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,10 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  photoURL: { type: String, default: '' },  // Url de foto de perfil
+  role: { type: String, default: 'user' }, // Rol del usuario, útil para permisos
   createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date },
 });
 
 module.exports = mongoose.model('User', UserSchema);
